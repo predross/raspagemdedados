85d2de5ec3b61893b4b0f17e444c886ac110df34
Pedro Igor <90657856+predross@users.noreply.github.com>
2023-06-15 16:09:55 -0300

    Update coleta.py
    o selenium passou por atualizações recentes, não é necessário que configure o drive etc... 
    Leia a documentação: https://www.selenium.dev/pt-br/documentation/webdriver/getting_started/upgrade_to_selenium_4/

--- coleta.py
+++ coleta.py
@@ -1,48 +1,39 @@
-# Importando as bibliotecas
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from bs4 import BeautifulSoup
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-import requests
-from bs4 import BeautifulSoup
 from time import sleep
-import re
 
 url = "https://divulgacandcontas.tse.jus.br/divulga/#/estados/2020/2030402020/PR/municipios"
 
-#Configurando o navegador
+# Configurando o navegador
 options = webdriver.ChromeOptions()
-#options.add_argument("--headless")
-options.add_argument( 'options.add_argument( window-size=1200 x 600 )' )
+# options.add_argument("--headless")
+options.add_argument('--window-size=1200,600')
 
-#web-driver, que instalado o navegador padrão utilizado na maquina do cliente
-driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), chrome_options=options)
+# Web driver, que instala o navegador padrão utilizado na máquina do cliente
+driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()), options=options)
 
 def Exec_scrapping():
         
-    #Abrir o navegador
-    resposta = driver.get(url)
-    resposta
+    # Abrir o navegador
+    driver.get(url)
     print('- Iniciou o Chrome')
     sleep(6)
     
-    def fist_scrapping():
+    def first_scrapping():
         
-        # click na imagem para ir na página da noticia
-        titulo = driver.find_element(By.CLASS_NAME, 'elementor-post__thumbnail')
-        print('PRint do element',titulo)
+        #href="#/candidato/2020/2030402020/76910/160001051906"
+        # Clicar na imagem para ir na página da notícia
+        titulo = driver.find_element(By.CLASS_NAME, 'img-thumbnail')
+        print('Print do elemento', titulo)
         titulo.click()
-        print('-- click in title ')
+        print('-- click in title')
         sleep(3)
     
-    fist_scrapping()
-    
-    #get url
-    
-        
+    first_scrapping()
     
     def full_scrapping():
            
@@ -51,29 +42,22 @@ def Exec_scrapping():
         
         print('---------------------------------------------')
         
-        titulo = page_source.find('h1', class_ = '')
-        fort_tirulo = titulo.text
-        print(' --TITULO : ',fort_tirulo)
+        titulo = page_source.find('h1', class_='ng-binding')
+        # fort_tirulo = titulo.text
+        # print(' --TITULO : ',fort_tirulo)
         
-        sub_titulo = page_source.find('h2', class_ = '')
-        format_sub = sub_titulo.text
-        print(' --SUB TITULO : ',format_sub)
+        sub_titulo = page_source.find('h2', class_='text-center')
+        # format_sub = sub_titulo.text
+        # print(' --SUB TITULO : ',format_sub)
         
-        get_text = page_source.find('div', class_ = 'entry-')
-        format_text = get_text.text
-        print(' --ARTIGO : ',format_text)
+        get_text = page_source.find('h4', class_='ng-binding')
+        # format_text = get_text.text
+        # print(' --ARTIGO : ',format_text)
         
         print('---------------------------------------------')
 
         sleep(7200)
     full_scrapping()
         
-        
-        
 while True:
-    
-    Exec_scrapping() 
-
-
-
-    
+    Exec_scrapping()
