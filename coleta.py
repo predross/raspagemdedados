b3f370ea3d346d7e6ac6117d91d1ecaf5a762d6f
Pedro Igor <90657856+predross@users.noreply.github.com>
2023-06-16 15:20:37 -0300

    Update coleta.py

--- coleta.py
+++ coleta.py
@@ -1,64 +1,65 @@
 import pandas as pd
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
-
-# Configurando o navegador
-options = webdriver.ChromeOptions()
-options.add_argument("--window-size=1200x600")
+import openpyxl
 
 # Configurando o ChromeDriver usando o ChromeDriverManager
-driver = webdriver.Chrome(service=webdriver.chrome.service.Service(ChromeDriverManager().install()), options=options)
+driver_path = ChromeDriverManager().install()
+service = Service(driver_path)
 
-url = "COLOQUE AQUI SUA URL"
+# Configurando as opções do navegador
+options = webdriver.ChromeOptions()
+options.add_argument("--window-size=1200x600")
+options.add_experimental_option('excludeSwitches', ['enable-logging'])  # Adicionado para suprimir as mensagens de erro
 
-driver.get(url)
-sleep(3)
+# Inicializando o navegador
+driver = webdriver.Chrome(service=service, options=options)
 
-
-todos_municipios = driver.find_elements(By.CSS_SELECTOR, "body > div.conteudo > div.container-fluid.dvg-main-wrap > div > div > section:nth-child(3) > div > div > table > tbody > tr:nth-child(2)")
-
-municipios_data = []
-
-for municipio in todos_municipios:
-    
-    # Clicando no município para obter as informações das pessoas
-    municipio.click()
+def collect_data(url):
+    driver.get(url)
     sleep(3)
-    
 
-    todas_pessoas = driver.find_elements(By.CSS_SELECTOR, "body > div.conteudo > div.container-fluid.dvg-main-wrap > div > div > section:nth-child(3) > div > div > table.table.table-hover.visible-md.visible-lg > tbody > tr:nth-child(1) > td:nth-child(1) > a")
-    
-    
-    for pessoa in todas_pessoas:
-        
-        # Obtendo as informações da pessoa
-        pessoa_link = pessoa.get_attribute("href")
-        municipios_data.append(pessoa_link)
-        
-        for link in pessoa_link:
-            
-            driver.get(pessoa_link)
+    # Localizando todos os municípios
+    todos_municipios = driver.find_elements(By.CSS_SELECTOR, "body > div.conteudo > div.container-fluid.dvg-main-wrap > div > div > section:nth-child(3) > div > div > table > tbody > tr > td > a > div")
+
+    municipios_data = []
+
+    for municipio in todos_municipios[:399]:  # Ajuste para percorrer 399 cidades
+        # Clicando no município para obter as informações dos candidatos
+        municipio.click()
+        sleep(3)
+
+        # Localizando os candidatos de cada tipo (prefeito, vice, vereador)
+        tipos_candidatos = driver.find_elements(By.CSS_SELECTOR, "body > div.conteudo > div.container-fluid.dvg-main-wrap > div > div > div > table > tbody > tr > td:nth-child(1) > select > option")
+
+        for tipo in tipos_candidatos:
+            # Clicando no tipo de candidato para obter as informações
+            tipo.click()
             sleep(3)
-            
-            # Pegar as informação pessoais
-            
-            
-            ###############################
-            driver.back()
-        
-        pessoa_data = pessoa.text
-        municipios_data.append(pessoa_data)
-    
 
-    driver.back()
-    sleep(2)
+            # Obtendo as informações do candidato
+            nome_completo = driver.find_element(By.CSS_SELECTOR, "body > div.conteudo > div.container-fluid.dvg-main-wrap > div > div:nth-child(1) > section:nth-child(3) > div > div.col-md-7 > div.row > div:nth-child(1) > div > div.text > h3").text
+            site = ""
+            if driver.find_elements(By.CSS_SELECTOR, "body > div.conteudo > div.container-fluid.dvg-main-wrap > div > div:nth-child(1) > section:nth-child(3) > div > div.col-md-7 > div.row > div:nth-child(11) > div > div.text > h3.social-midia.ng-scope > a"):
+                site = driver.find_element(By.CSS_SELECTOR, "body > div.conteudo > div.container-fluid.dvg-main-wrap > div > div:nth-child(1) > section:nth-child(3) > div > div.col-md-7 > div.row > div:nth-child(11) > div > div.text > h3.social-midia.ng-scope > a").get_attribute("href")
+            total_recursos_recebidos = driver.find_element(By.CSS_SELECTOR, "body > div.conteudo > div.container-fluid.dvg-main-wrap > div > div:nth-child(2) > section:nth-child(2) > div > div:nth-child(2) > div:nth-child(2) > div > div > h2.ng-binding").text
+            total_despesas = driver.find_element(By.CSS_SELECTOR, "body > div.conteudo > div.container-fluid.dvg-main-wrap > div > div:nth-child(2) > section:nth-child(2) > div > div:nth-child(3) > div:nth-child(3) > div > div > h3.ng-binding").text
 
+            # Salvando as informações em uma lista
+            municipios_data.append([nome_completo, site, total_recursos_recebidos, total_despesas])
 
+    return municipios_data
+
+base_url = "https://divulgacandcontas.tse.jus.br/divulga/#/estados/2020/2030402020/PR/municipios"
+dados_municipios = collect_data(base_url)
+
+# Salvando os dados em um arquivo Excel
+arquivo_excel = "dados.xlsx"
+df = pd.DataFrame(dados_municipios, columns=["Nome Completo", "Site", "Total Recursos Recebidos", "Total Despesas"])
+df.to_excel(arquivo_excel, index=False)
+
+# Fechando o navegador
 driver.quit()
-
-
-df = pd.DataFrame(municipios_data, columns=["Informacoes"])
-print(df)
